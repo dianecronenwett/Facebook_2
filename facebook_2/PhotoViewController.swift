e09796ba78de40536d4ca30f516975b4a0c52d2f
diane <dianecronenwett@gmail.com>
2014-11-03 12:02:01 -0800

    update

---
+++ facebook_2/PhotoViewController.swift
@@ -0,0 +1,9 @@
+//
+//  PhotoViewController.swift
+//  facebook
+//
+//  Created by diane cronenwett on 11/2/14.
+//  Copyright (c) 2014 dianesorg. All rights reserved.
+//
+
+import Foundation
--- facebook_2/facebook/FeedScrollViewController.swift
+++ facebook_2/facebook/FeedScrollViewController.swift
@@ -10,26 +10,35 @@ import UIKit
 
 class FeedScrollViewController: UIViewController, UIScrollViewDelegate {
     @IBOutlet weak var feedImage: UIImageView!
-    @IBOutlet weak var blackDressImage: UIImageView!
-    @IBAction func OnPhotoTap(sender: UITapGestureRecognizer) {
-        println("hi")
-    }
+    @IBOutlet weak var composeImage: UIImageView!
     @IBOutlet weak var scrollView: UIScrollView!
-    
+    @IBOutlet weak var blackDressImage: UIImageView!
+    @IBAction func onDressTap(sender: UITapGestureRecognizer) {
+        //println("dresstapped")
+         performSegueWithIdentifier("blackDressSegue", sender: self)
+    }
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        scrollView.delegate = self
-        scrollView.contentSize = CGSize (width: 320, height:feedImage.frame.height
-        )
+   var containerHeight = feedImage.frame.height + composeImage.frame.height
+    scrollView.contentInset.bottom = 180
+        
+   scrollView.delegate = self
+        scrollView.contentSize.height = containerHeight
+       
     }
-
+   
+    //Add prepare for segue here
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
     
+    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer!, shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer: UIGestureRecognizer!) -> Bool {
+        return true
+    }
 
    
 }
